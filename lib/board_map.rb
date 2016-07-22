e12f7bf0f69a0552503ac773daa8b7744a0b192f
Ruslana Kiverskaya <ruslana.kiverskaya@gmail.com>
2016-07-22 16:03:49 +0300

    Rendering of board maps

--- lib/board_map.rb
+++ lib/board_map.rb
@@ -1,4 +1,38 @@
 class BoardMap
   BOMB_CHAR  = '*'
   EMPTY_CHAR = '.'
+
+  def initialize(size, mines)
+    @mines = mines
+    @board = Array.new(size.to_i) { Array.new(size.to_i, 0) }
+  end
+
+  def to_s
+    rendering_board_map
+    @board.each do |row|
+      row.map! { |cell| cell = cell == 0 ? EMPTY_CHAR : cell }
+      row.push("\n")
+    end
+    @board.join
+  end
+
+  def rendering_board_map
+    @mines.each do |mine|
+      column, row = mine
+      @board[row][column] = BOMB_CHAR
+
+      neighboring_rows(row).each do |row|
+        [column, column - 1, column + 1].each do |column|
+          @board[row][column] += 1 if @board[row][column] && @board[row][column] != BOMB_CHAR
+        end
+      end
+    end
+  end
+
+  def neighboring_rows(row)
+    rows = [row]
+    rows.push(row - 1) if @board[row - 1]
+    rows.push(row + 1) if @board[row + 1]
+    rows
+  end
 end
--- lib/mine_sweeper.rb
+++ lib/mine_sweeper.rb
@@ -1,3 +1,28 @@
+require 'yaml'
+
 class MineSweeper
   BOARDS_FILE = 'boards.yml'
+
+  def initialize(kind)
+    @kind = kind
+    ensure_level_is_correct
+  end
+
+  def render(file)
+    board = get_board(@kind)
+    map = BoardMap.new(board["size"], board["mines"]).to_s
+    file.write(map)
+  end
+
+  def ensure_level_is_correct
+    raise ArgumentError unless %w(empty easy medium hard).include?(@kind)
+  end
+
+  def load_boards
+    @boards ||= YAML.load(File.read(BOARDS_FILE))
+  end
+
+  def get_board(kind)
+    load_boards.detect{ |x| x["type"] == kind }
+  end
 end
